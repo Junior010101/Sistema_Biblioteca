7a3608c6237e1e2fc63a6171f5dcbe10e94ce009
Junior010101 <marcondesjunior2209@gmail.com>
2024-09-18 19:52:34 -0300

    commit

--- MyConsoleLibrary/obj/Debug/net8.0/MyConsoleLibrary.AssemblyInfo.cs
+++ MyConsoleLibrary/obj/Debug/net8.0/MyConsoleLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3acb92df9b8c9a0d6bcdb97877fbfd89c2c1c004")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
